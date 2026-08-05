ef3a8cf04f632957a5bea78a3f33a6ac9cbd0b1c
Madhusanka S <205752706+sankabuilds@users.noreply.github.com>
2026-08-05 20:38:18 +0530

    fix: clippy warnings

--- src/handlers/instagram.rs
+++ src/handlers/instagram.rs
@@ -3,31 +3,26 @@ use std::time::Duration;
 use xxer::site::{common::Site, instagram::ViewType};
 
 use crate::args::{Cli, InstagramBookmarksArgs};
-use xxer::{
-    downloader::instagram::DownloaderOptions,
-    site::instagram::{Instagram, Slide},
-};
+use xxer::{downloader::instagram::DownloaderOptions, site::instagram::Instagram};
 
 pub async fn bookmarks(
     instagram_bookmarks_args: &InstagramBookmarksArgs,
     args: &Cli,
 ) -> Result<()> {
     if let Some(cookie_file) = &args.cookie {
-        let slides: Vec<Slide>;
-
-        if instagram_bookmarks_args.all {
+        let slides = if instagram_bookmarks_args.all {
             eprintln!("Gathering all your bookmarks. This may take some time!");
 
-            slides = Instagram::new(cookie_file)
+            Instagram::new(cookie_file)
                 .get(ViewType::Bookmarks, None)
                 .await
-                .context("failed to get the ViewType")?;
+                .context("failed to get the ViewType")?
         } else {
-            slides = Instagram::new(cookie_file)
+            Instagram::new(cookie_file)
                 .get(ViewType::Bookmarks, Some(instagram_bookmarks_args.limit))
                 .await
-                .context("failed to get the ViewType")?;
-        }
+                .context("failed to get the ViewType")?
+        };
 
         DownloaderOptions::new()
             .timeout(Duration::from_millis(instagram_bookmarks_args.timeout))
--- src/handlers/x.rs
+++ src/handlers/x.rs
@@ -3,28 +3,26 @@ use std::time::Duration;
 
 use xxer::{
     downloader::x::DownloaderOptions,
-    site::x::{Slide, ViewType, XTwitter},
+    site::x::{ViewType, XTwitter},
 };
 
 use crate::args::{Cli, XBookmarksArgs};
 
 pub async fn bookmarks(x_bookmarks_args: &XBookmarksArgs, args: &Cli) -> Result<()> {
     if let Some(cookie_file) = &args.cookie {
-        let slides: Vec<Slide>;
-
-        if x_bookmarks_args.all {
+        let slides = if x_bookmarks_args.all {
             eprintln!("Gathering all your bookmarks. This may take some time!");
 
-            slides = XTwitter::new(cookie_file)
+            XTwitter::new(cookie_file)
                 .get(ViewType::Bookmarks, None)
                 .await
-                .context("failed to get the ViewType")?;
+                .context("failed to get the ViewType")?
         } else {
-            slides = XTwitter::new(cookie_file)
+            XTwitter::new(cookie_file)
                 .get(ViewType::Bookmarks, Some(x_bookmarks_args.limit))
                 .await
-                .context("failed to get the ViewType")?;
-        }
+                .context("failed to get the ViewType")?
+        };
 
         DownloaderOptions::new()
             .timeout(Duration::from_millis(x_bookmarks_args.timeout))
